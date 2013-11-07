3d042926469add52fab19965a37bae4f4f8173cf
Meetu Maltiar <meetu.maltiar@gmail.com>
2013-11-07 16:21:39 +0530

    Added the code for cluster client over the clustered router

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 AkkaCluster
 ===========
 
-Experiments on Akka Cluster
+Bring up two nodes in the cluster with the following commands:
+1. sbt "run-main com.cisco.akka.cluster.ClusterServerApp 2551"
+2. sbt "run-main com.cisco.akka.cluster.ClusterServerApp 2552"
+
+Then use the client to send to the cluster via cluster router
+sbt "run-main com.cisco.akka.cluster.ClusterClientApp"
+
+You will see the message flowing in all the nodes in the cluster.
--- src/main/scala/com/cisco/akka/cluster/ClusterServerApp.scala
+++ src/main/scala/com/cisco/akka/cluster/ClusterServerApp.scala
@@ -1,33 +1,25 @@
 package com.cisco.akka.cluster
 
+import com.typesafe.config.ConfigFactory
+
 import akka.actor._
 import akka.cluster.Cluster
-import akka.cluster.ClusterEvent._
-import akka.routing._
+import akka.cluster.routing._
 import akka.contrib.pattern._
-import com.typesafe.config.ConfigFactory
+import akka.routing._
 
 object ClusterServerApp extends App {
   if (args.nonEmpty) System.setProperty("akka.remote.netty.tcp.port", args(0))
   val system = ActorSystem("ClusterSystem")
   val router = system.actorOf(Props[Worker].withRouter(RoundRobinRouter(nrOfInstances = 4)), "workers")
-  Cluster(system).subscribe(router, classOf[ClusterDomainEvent])
-  ClusterReceptionistExtension(system).registerService(router)
+  Cluster(system)
+  val clusterRouterSettings = ClusterRouterSettings(totalInstances = 100, routeesPath = "/user/workers", allowLocalRoutees = true, useRole = None)
+  val clusterAwareWorkerRouter = system.actorOf(Props.empty.withRouter(ClusterRouterConfig(RoundRobinRouter(), clusterRouterSettings)), name = "workerRouter")
+  ClusterReceptionistExtension(system).registerService(clusterAwareWorkerRouter)
 }
 
 class Worker extends Actor {
   def receive = {
-    case msg: String => println(msg)
+    case msg: String => println(msg + " " + self.path)
   }
-}
-
-object ClusterClientApp extends App {
-  val conf = ConfigFactory.load("client-application.conf")
-  val system = ActorSystem("client", conf)
-  val initialContacts = Set(
-    system.actorSelection("akka.tcp://ClusterSystem@127.0.0.1:2551/user/receptionist"),
-    system.actorSelection("akka.tcp://ClusterSystem@127.0.0.1:2552/user/receptionist"))
-  val clusterClient = system.actorOf(ClusterClient.props(initialContacts))
-  Thread.sleep(3000)
-  clusterClient ! ClusterClient.Send("/user/workers", "hello", localAffinity = true)
 }
